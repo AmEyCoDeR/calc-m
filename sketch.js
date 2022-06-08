8d14b5bac465eef1f09c85379f2da583d48fa8ae
AmEyCoDeR <99733292+AmEyCoDeR@users.noreply.github.com>
2022-06-08 18:11:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,75 @@
+function cleara() {
+    document.getElementById("valueoo").value = "";
+}
+
+function multiply() {
+    document.getElementById("valueoo").value += "*";
+}
+
+function substract() {
+    document.getElementById("valueoo").value += "-";
+}
+
+function add() {
+    document.getElementById("valueoo").value += "+";
+}
+
+function divide() {
+    document.getElementById("valueoo").value += "/";
+}
+
+function zero() {
+    document.getElementById("valueoo").value += "0";
+}
+
+function one() {
+    document.getElementById("valueoo").value += "1";
+}
+
+function two() {
+    document.getElementById("valueoo").value += "2";
+}
+
+function three() {
+    document.getElementById("valueoo").value += "3";
+}
+
+function four() {
+    document.getElementById("valueoo").value += "4";
+}
+
+function five() {
+    document.getElementById("valueoo").value += "5";
+}
+
+function six() {
+    document.getElementById("valueoo").value += "6";
+}
+
+function seven() {
+    document.getElementById("valueoo").value += "7";
+}
+
+function eight() {
+    document.getElementById("valueoo").value += "8";
+}
+
+function nine() {
+    document.getElementById("valueoo").value += "9";
+}
+
+function result() {
+    document.getElementById("valueoo").value = eval(document.getElementById("valueoo").value);
+}
+
+function dot(){
+    document.getElementById("valueoo").value += ".";
+}
+
+function bracketleft(){
+    document.getElementById("valueoo").value += "(";
+}
+
+function bracketright(){
+    document.getElementById("valueoo").value += ")";
+}
